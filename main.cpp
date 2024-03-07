9b80eeafdecfbf9253b3069a4d940d32a508d8f6
Tejas A Kumar <109240619+Tejasakumar@users.noreply.github.com>
2024-03-07 18:47:21 +0530

    Update_1 main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int i=1;
     int a[]={0,1,0,0,0,0,0,0,0,0,0};
-    for(int i=1;i<9;i++){
+    for(int i=1;i<19;i++){
         cout<<a[i]<<endl;
         a[i+1]=a[i]+a[i-1];
     }
